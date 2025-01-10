9486a7ef977c745840481710ffab0e43fcd4779c
2025ericl944 <95231753+2025ericl944@users.noreply.github.com>
2025-01-10 20:56:59 +0000

    I started working on the display and the game.

---
+++ SnacheDisplay.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.io.File;
+
+public class SnacheDisplay extends JPanel implements ActionListener, KeyListener{
+    int timer = 0;
+    Graphics2D g2;
+    Timer refreshRate = new Timer(2, this);
+    boolean right, left, up, down;
+    
+}
